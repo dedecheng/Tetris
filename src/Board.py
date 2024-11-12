9fb436e310141b29eac6d02bd921b7bcd680613e
ray <chang.anjui@gmail.com>
2024-11-12 17:35:19 +0800

    Board define

---
+++ src/Board.py
@@ -0,0 +1,11 @@
+import Block
+
+class Board():
+    def __init__(self):
+        self.width = 10
+        self.height = 20
+        self.board=[]
+        self.active_block = Block()
+
+    def rotate(self,Block):
+        pass
